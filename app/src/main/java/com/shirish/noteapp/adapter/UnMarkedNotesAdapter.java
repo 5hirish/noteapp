ce695a7d33d4eaf19df4c16a24da6db1674bea1c
Shirish Kadam <shirishkadam35@yahoo.com>
2018-06-09 23:48:31 +0530

    Added Recycler view adapters

---
+++ app/src/main/java/com/shirish/noteapp/adapter/UnMarkedNotesAdapter.java
@@ -0,0 +1,66 @@
+package com.shirish.noteapp.adapter;
+
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.shirish.noteapp.R;
+import com.shirish.noteapp.model.Note;
+
+import java.util.List;
+
+public class UnMarkedNotesAdapter extends RecyclerView.Adapter<UnMarkedNotesAdapter.NoteViewHolder> {
+
+    private List<Note> noteList;
+
+    public UnMarkedNotesAdapter(List<Note> noteList) {
+        this.noteList = noteList;
+    }
+
+    @Override
+    public NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final NoteViewHolder holder, int position) {
+        Note note = noteList.get(position);
+        holder.cbNoteStatus.setEnabled(note.isNoteChecked());
+        holder.tvNoteContent.setText(note.getNoteContent());
+        holder.tvNoteCategory.setText(note.getNoteCategory());
+
+        // Checkbox listener
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+
+    /*
+
+    public void addNotes(List<Note> noteList) {
+        this.noteList = noteList;
+        notifyDataSetChanged();
+    }
+
+    */
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvNoteContent, tvNoteCategory;
+        private AppCompatCheckBox cbNoteStatus;
+
+        NoteViewHolder(View view) {
+            super(view);
+
+            cbNoteStatus = (AppCompatCheckBox) view.findViewById(R.id.cbNoteMarked);
+            tvNoteContent = (TextView) view.findViewById(R.id.tvNoteContent);
+            tvNoteCategory = (TextView) view.findViewById(R.id.tvNoteCategory);
+        }
+    }
+}
